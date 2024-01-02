5a77630abba09d6c016bd0a57ca7300e715cd28d
Osama daoud <osamadaoud66@gmail.com>
2024-01-02 03:14:46 +0300

    complete the Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2; 
+let num2 = 31; 
+ 
+let multiply = num1*num2;
+ 
+const random = Math.ceil(Math.random());
+ 
+let num3 = 9;
+let num4=5; 
+const mod = num3%num4 ; 
 
-// Write your code here
+let max =  Math.max(20, 2 ,3,4,5,6);
